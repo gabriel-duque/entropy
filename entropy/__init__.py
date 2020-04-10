9a6edff23e7545e0b5bc30d5ca41d4c3798acd23
Gabriel Duque <gabriel.duque@lse.epita.fr>
2020-04-10 20:08:20 +0200

    src: add imports to __init__.py
    Signed-off-by: Gabriel Duque <gabriel.duque@lse.epita.fr>

--- entropy/__init__.py
+++ entropy/__init__.py
@@ -1,4 +1,10 @@
 """
     ``entropy`` is a library which helps create return-oriented programming chains.
 """
+
 __version__: str = "0.0.1"
+
+from entropy import elf
+from entropy import finder
+from entropy import gadget
+from entropy import log
