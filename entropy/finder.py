82fec08ef190996e7f51b665e9abf9422bf3ec80
Gabriel Duque <gabriel.duque@lse.epita.fr>
2020-04-10 20:53:30 +0200

    doc: add docstrings to finder module
    Signed-off-by: Gabriel Duque <gabriel.duque@lse.epita.fr>

--- entropy/finder.py
+++ entropy/finder.py
@@ -17,7 +17,11 @@ class Finder:
     __raw: bytearray
 
     def __init__(self, raw: bytearray) -> None:
-        """Initialize our finder with the raw bytes of our file."""
+        """Initialize our finder with the raw bytes of our file.
+
+        :param raw: raw input file bytes
+        :type raw: bytearray
+        """
         self.__raw: bytearray = raw
 
     def __call__(
@@ -25,11 +29,22 @@ class Finder:
     ) -> List[gadget.Gadget]:
         """Search for gadgets in our segments and concatenate all
         returned lists.
+
+        :param executable_segments: iterator over executable segment program headers
+        :type executable_segments: Iterator[elf.Phdr64LSB]
+        :return: list of found gadgets
+        :rtype: List[gadget.Gadget]
         """
         return functools.reduce(
             operator.add, map(self.__find_gadgets, executable_segments)
         )
 
     def __find_gadgets(self, segment: elf.Phdr64LSB) -> List[gadget.Gadget]:
-        """Search for gadgets in a specific segment."""
-        return []
+        """Search for gadgets in a specific segment.
+
+        :param segment: program header of the analyzed segment
+        :type segment: elf.Phdr64LSB
+        :return: list of gadgets found in segment
+        :rtype: List[gadget.Gadget]
+        """
+        return list()
