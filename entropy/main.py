3675f329ae3886a82dbf60e0abefc3fdbaf93793
Gabriel Duque <gabriel.duque@lse.epita.fr>
2020-04-10 20:07:57 +0200

    src: rename entropy.py to main.py and fix imports
    Signed-off-by: Gabriel Duque <gabriel.duque@lse.epita.fr>

--- entropy/main.py
+++ entropy/main.py
@@ -9,8 +9,12 @@
 
 import argparse
 
-import entropy.elf
-import entropy.log
+from typing import List
+
+from entropy import elf
+from entropy import finder
+from entropy import gadget
+from entropy import log
 
 
 def main() -> None:
@@ -20,5 +24,11 @@ def main() -> None:
     parser.add_argument("elf")
     args: argparse.Namespace = parser.parse_args()
 
-    entropy.log.info(f"parsing file {args.elf}")
-    input_elf: entropy.elf.ELF = entropy.elf.ELF.from_file_name(args.elf)
+    log.info(f"parsing file {args.elf}")
+    input_elf: elf.ELF = elf.ELF.from_file_name(args.elf)
+    gadget_finder: finder.Finder = finder.Finder(input_elf.raw)
+    log.info(f"searching for gadgets")
+    gadgets: List[gadget.Gadget] = gadget_finder(
+        input_elf.iter_executable_segments()
+    )
+    log.info(f"found {len(gadgets)} gadgets")
